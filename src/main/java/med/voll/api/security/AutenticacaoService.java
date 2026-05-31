66a689986a3f404302fba8ca246ec98083ecf24d
Luiz Gustavo <gluizdev@gmail.com>
2026-05-31 16:47:20 -0400

    feat: implementa repositório de usuário e serviço de autenticação

---
+++ src/main/java/med/voll/api/security/AutenticacaoService.java
@@ -0,0 +1,22 @@
+package med.voll.api.security;
+
+import med.voll.api.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(login);
+    }
+}
