b3c9a4f590038dbcd74167411a70985adf96d865
Luiz Gustavo <gluizdev@gmail.com>
2026-06-13 17:19:35 -0400

    adiciona biblioteca jwt e configura geração de token

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.DadosLogin;
+import med.voll.api.infra.security.TokenService;
+import med.voll.api.model.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     private AuthenticationManager authenticationManager;
+    private TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager authenticationManager){
+    public AutenticacaoController(AuthenticationManager authenticationManager, TokenService tokenService){
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
@@ -25,7 +29,7 @@ public class AutenticacaoController {
         var token = new UsernamePasswordAuthenticationToken(dadosLogin.login(), dadosLogin.senha());
         var authentication = authenticationManager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
